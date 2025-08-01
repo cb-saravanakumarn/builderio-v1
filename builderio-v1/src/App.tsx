d219fd36f08cc86431315ecf621a9edc6ee9e276
cb-saravanakumarn <saravanakumar.n@chargebee.com>
2025-08-01 13:08:39 +0530

    running

--- builderio-v1/src/App.tsx
+++ builderio-v1/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
-import "@chargebee/sting-react/sting-react.css";
+import "@chargebee/sting-react/sting-react.scss";
 import { BuilderComponent, builder } from "@builder.io/react";
 import "./lib/builder-registry";
+import { SButton } from "@chargebee/sting-react";
 builder.init("6fc8a239bb834197943148b3e4ca1946");
 
 function App() {
   return (
     <div className="App">
+      {/* <SButton>Click me</SButton> */}
       <BuilderComponent
         model="page"
         content={undefined} // This will be populated by Builder
